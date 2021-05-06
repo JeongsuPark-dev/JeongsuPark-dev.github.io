8908ead0de388ba68a786ecf583812001f43806a
park jeongsu <integer613@gmail.com>
2021-05-07 02:32:44 +0900

    Build deployment(21.05.07)

--- src/components/InnerHeader.js
+++ src/components/InnerHeader.js
@@ -10,7 +10,7 @@ export default class InnerHeader extends Component {
             <div className="App-mainbody">
                 <div style={{paddingTop : "30px",backgroundColor: "rgb(2,29,46)"}}></div>
                 <Body Title={"신규 작성중인 페이지로 실시간 업데이트 중입니다."} />
-                <Body Title={"마지막 업데이트 일자 : 2021-04-29 18:00"} />
+                <Body Title={"마지막 업데이트 일자 : 2021-05-07 02:35"} />
             </div>
         );
     }
